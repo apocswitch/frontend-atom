fcb35b0d9d0bb5fe7e72c5cdd94e0f40d0cdea7e
eherna11 <ehernandezj.externo@coopeuch.cl>
2025-03-31 19:34:43 -0300

    soporte para pwa

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,14 @@ import { AppComponent } from "./app/app.component";
 import { appConfig } from "./app/app.config";
 import { provideHttpClient, withInterceptors } from "@angular/common/http";
 import { authInterceptor } from "./app/interceptors/auth.interceptor";
+import { provideServiceWorker } from "@angular/service-worker";
+import { environment } from "./environments/environment";
 
 bootstrapApplication(AppComponent, {
     ...appConfig,
     providers: [
       provideHttpClient(withInterceptors([authInterceptor])),
-      ...(appConfig.providers || [])
+      ...(appConfig.providers || []),
+      environment.production ? provideServiceWorker("ngsw-config.json") : [],
     ]
   }).catch((err) => console.error(err));
